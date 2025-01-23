6c37bcd6be55995f6d2e8c2a829e1dfadd86334e
EvanPulgino <evan.pulgino@gmail.com>
2025-01-22 22:46:38 -0800

    Building some more elements

--- js/index.js
+++ js/index.js
@@ -1,8 +1,21 @@
+function getSkillElement(skill) {
+	return `<div class="skill">${skill}</div>`;
+}
+
 function populateContent(content) {
+	// Profile
 	document.getElementById('name').textContent = `${content.name.first} ${content.name.last}`;
 	document.getElementById('location').textContent = `${content.location.city}, ${content.location.state}`;
 	document.getElementById('phone-number').textContent = `${content.contact.phone_number}`;
 	document.getElementById('email-address').textContent = `${content.contact.email_address}`;
+
+	// Bio
+	document.getElementById('bio').textContent = `${content.bio}`;
+
+	// Skills
+	Object.values(content.skills).forEach(skill => {
+		document.getElementById('skills').insertAdjacentHtml('beforeend', getSkillElement(skill));
+	})
 }
 
 function populateDataOnLoad() {
