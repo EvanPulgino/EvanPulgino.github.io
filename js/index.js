803395a998d210f26959a58e94cc76b686a75a7e
Evan Pulgino <evan.pulgino@gmail.com>
2025-01-22 17:03:13 -0800

    move json data to JS file

--- js/index.js
+++ js/index.js
@@ -1,4 +1,20 @@
-import content from "./data/content.json" assert { type: 'json' };
+const content = `
+	{
+		"content": {
+		"name": {
+			"first": "Evan",
+			"last": "Pulgino"
+		},
+		"location": {
+			"city": "Seattle",
+			"state": "WA"
+		},
+		"contact": {
+			"phone-number": "412-512-4723",
+			"email-address": "evan.pulgino@pm.me"
+		}
+	}
+`;
 
 function populateDataOnLoad() {
 	console.log(content);
